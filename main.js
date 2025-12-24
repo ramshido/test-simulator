e0280e141fcf644d05d7fec5bdd36850f5aa0a38
Khao Kai <Ramza@gmail.com>
2025-12-24 16:37:31 +0300

    [#4](chore): Created btns for changing cards background

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-// 1 
-const productCard = document.querySelector('.product-card');
-const productCardBtn = document.querySelector('.product-card__btn');
+// 1 Переключение цвета
+const productCardBtns = document.querySelectorAll('.product-card__btn');
 
-productCard.addEventListener( "DOMContentLoaded", () => {
-	productCard.classList.toggle('.product-card_bg-blue');
+productCardBtns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		btn.classList.toggle('product-card_bg-blue');
+	});
 })
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('this is addEventList');
-// });
 
+// 2 Покарска карточки
+const blueColorHash = '#0000FF';
+const greenColorHash = '#00FF00';
 
-setTimeout(() => {
-	console.log('Timer');
-}, 2000)
+const allProductCards = document.querySelectorAll('.product-card');
+const firstProductCard = document.querySelector('.product-card-first');
+const changeColorFirstCardBtn = document.querySelector('#change-color-first-card'); 
+const changeColorAlltCardBtn = document.querySelector('#change-color-all-cards'); 
+
+changeColorFirstCardBtn.addEventListener('click', () => {
+	firstProductCard.style.backgroundColor = blueColorHash;
+})
+
+changeColorAlltCardBtn.addEventListener('click', () => {
+	allProductCards.forEach((card) => {
+		card.style.backgroundColor = greenColorHash;
+	})
+})
+
+// 3 Открыть google
+const openGoogleBtn = document.querySelector('#open-google');
+
+openGoogleBtn.addEventListener('click', () => {
+	window.open('https://google.com')
+})
