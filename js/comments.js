95bf011c20dd5825e1daf60c86589df177560b58
Khao Kai <Ramza@gmail.com>
2025-12-30 19:45:30 +0300

    [#7](feat): finished 2-level tasks

---
+++ js/comments.js
@@ -0,0 +1,102 @@
+export const userComments = [
+	{
+		postId: 1,
+		id: 1,
+		name: "id labore ex et quam laborum",
+		email: "Eliseo@gardner.biz",
+		body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\n" +
+			"tempora quo necessitatibus\ndolor quam autem quasi\n" +
+			"reiciendis et nam sapiente accusantium"
+	},
+	{
+		postId: 1,
+		id: 2,
+		name: "quo vero reiciendis velit similique earum",
+		email: "Jayne_Kuhic@sydney.com",
+		body: "est natus enim nihil est dolore omnis voluptatem numquam\n" +
+			"et omnis occaecati quod ullam at\n" +
+			"voluptatem error expedita pariatur\n" +
+			"nihil sint nostrum voluptatem reiciendis et"
+	},
+	{
+		postId: 1,
+		id: 3,
+		name: "odio adipisci rerum aut animi",
+		email: "Nikita@garfield.biz",
+		body: "quia molestiae reprehenderit quasi aspernatur\n" +
+			"aut expedita occaecati aliquam eveniet laudantium\n" +
+			"omnis quibusdam delectus saepe quia accusamus maiores nam est\n" +
+			"cum et ducimus et vero voluptates excepturi deleniti ratione"
+	},
+	{
+		postId: 1,
+		id: 4,
+		name: "alias odio sit",
+		email: "Lew@alysha.tv",
+		body: "non et atque\n" +
+			"occaecati deserunt quas accusantium unde odit nobis qui voluptatem\n" +
+			"quia voluptas consequuntur itaque dolor\n" +
+			"et qui rerum deleniti ut occaecati"
+	},
+	{
+		postId: 1,
+		id: 5,
+		name: "vero eaque aliquid doloribus et culpa",
+		email: "Hayden@althea.biz",
+		body: "harum non quasi et ratione\n" +
+			"tempore iure ex voluptates in ratione\n" +
+			"harum architecto fugit inventore cupiditate\n" +
+			"voluptates magni quo et"
+	},
+	{
+		postId: 2,
+		id: 6,
+		name: "et fugit eligendi deleniti quidem qui sint nihil autem",
+		email: "Presley.Mueller@myrl.com",
+		body: "doloribus at sed quis culpa deserunt consectetur qui praesentium\n" +
+			"accusamus fugiat dicta\n" +
+			"voluptatem rerum ut voluptate autem\n" +
+			"voluptatem repellendus aspernatur dolorem in"
+	},
+	{
+		postId: 2,
+		id: 7,
+		name: "repellat consequatur praesentium vel minus molestias voluptatum",
+		email: "Dallas@ole.me",
+		body: "maiores sed dolores similique labore et inventore et\n" +
+			"quasi temporibus esse sunt id et\n" +
+			"eos voluptatem aliquam\n" +
+			"aliquid ratione corporis molestiae mollitia quia et magnam dolor"
+	},
+	{
+		postId: 2,
+		id: 8,
+		name: "et omnis dolorem",
+		email: "Mallory_Kunze@marie.org",
+		body: "ut voluptatem corrupti velit\n" +
+			"ad voluptatem maiores\n" +
+			"et nisi velit vero accusamus maiores\n" +
+			"voluptates quia aliquid ullam eaque"
+	},
+	{
+		postId: 2,
+		id: 9,
+		name: "provident id voluptas",
+		email: "Meghan_Littel@rene.us",
+		body: "sapiente assumenda molestiae atque\n" +
+			"adipisci laborum distinctio aperiam et ab ut omnis\n" +
+			"et occaecati aspernatur odit sit rem expedita\n" +
+			"quas enim ipsam minus"
+	},
+	{
+		postId: 2,
+		id: 10,
+		name: "eaque et deleniti atque tenetur ut quo ut",
+		email: "Carmen_Keeling@caroline.name",
+		body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla\n" +
+			"quia quas dolores velit et non\n" +
+			"aut quia necessitatibus\n" +
+			"nostrum quaerat nulla et accusamus nisi facilis"
+	}
+];
+
--- js/homework-7.js
+++ js/homework-7.js
@@ -1,13 +1,15 @@
+import { userComments } from './comments.js';
+
 // Level 1
 // 1. Создать массив чисел от 1 до 10. Отфильтровать, чтобы получить массив чисел начиная с 5
 
 const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const filteredNumbersArray = numbersArray.slice(4);
+const numbersArrayFromFive = numbersArray.slice(4);
 
 // 2. Создать массив строк определенной темы (фильмы, книги) и проверить, есть ли в массиве что-то определенное.
 
 const tuesdayNightMovies = ['The Damned', 'George A. Romeros Resident Evil', 'Den of Thieves 2: Pantera', 'Survive', 'Get Away', 'Look Into My Eyes'];
-const hasSurviveMovieOnTuesday = tuesdayNightMovies.includes('Survive');
+const hasSurviveMovie = tuesdayNightMovies.includes('Survive');
 
 // 3. Написать функцию, которая принимает массив и реверсировать его. Два вышеуказанных массива перевернуть.
 
@@ -18,4 +20,57 @@ function reverseArray(array) {
 reverseArray(tuesdayNightMovies);
 reverseArray(numbersArray);
 
+// Level 2
+// 4. Добавить файл comments.js, в нём создать константу и в него поместить первые 10 объектов этого массива (https://jsonplaceholder.typicode.com/comments).
+// переменная должна быть названа по смыслу.
+
+// 6. Сделать константу экспортируемой, добавив перед "const" ключевое слово "export". Таким образом мы сможем внедрить переменную из comments.js в homework-7.js и работать с ней.
+// Когда мы введем название переменной, нам предложит импортировать ее - так и делаем.
+
+// 7. Вывести в консоль массив тех комментариев, почта пользователей которых содержит ".com"
+
+const userCommentsByDotCom = userComments.filter(userComment => {
+	if (userComment.email.includes('.com')) {
+		return userComment;
+	};
+});
+
+// 8. Перебрать массив таким образом, что бы пользователи с id меньше или равно 5 имели postId: 2, а те, у кого id больше 5, имели postId: 1
+
+userComments.map(userComment => {
+	if (userComment.id <= 5) {
+		userComment.postId = 2;
+	} else {
+		userComment.postId = 1;
+	};
+});
+
+// 9. Перебрать массив, что бы объекты состояли только из айди и имени
+
+const users = userComments.map(userComment => {
+	userComment = {
+		id: userComment.id,
+		name: userComment.name,
+	};
+	return userComment;
+});
+
+// 10. Перебираем массив, добавляем объектам свойство isInvalid и проверяем: если длина тела сообщения (body) больше 180 символов - устанавливаем true, меньше - false.
+
+userComments.map(userComment => {
+	if (userComment.body.length > 180) {
+		userComment.isInvalid = true;
+	} else {
+		userComment.isInvalid = false;
+	};
+});
+
+console.log(userComments);
+
+
+// Уровень 3:
+
+// 11. Почитать про метод массива reduce. Используя его, вывести массив почт и провернуть тоже самое с помощью метода map
+
+// 12. Почитать про методы toString(), join() и перебрав массив с задания №11, привести его к строке.
 
