c1dd018a8eef5fc741dfba4292bd0e53511bcff6
Khao Kai <Ramza@gmail.com>
2026-01-05 18:50:07 +0300

    [#8](feat): finished cards rendering function and getting cards number function

--- js/homework-8.js
+++ js/homework-8.js
@@ -3,23 +3,67 @@ import { productCards } from './product-cards.js';
 const productCardTemplate = document.getElementById('product-card-template');
 const productCardWrapper = document.querySelector('.product-wrapper');
 
-productCards.forEach(card => {
-	const cardClone = productCardTemplate.content.cloneNode(true);
+// 4. Изпользуя reduce() получить массив из объектов где ключ это название прдукта, значение же его описание
 
-	cardClone.querySelector('.product-card__img').setAttribute('src', `images/${card.img}.png`);
-	cardClone.querySelector('.product-card__img').setAttribute('alt', `${card.img}.png`);
-
-	cardClone.querySelector('.product-card__title').textContent = card.title;
-	cardClone.querySelector('.product-card__descr').textContent = card.descr;
-
-	card.structure.forEach(item => { 
-		const li = document.createElement('li');
-		li.classList.add('product-card__structure-item');
-		li.textContent = item;
-		cardClone.querySelector('.product-card__structure').appendChild(li); 
+const productProfile = productCards.reduce((acc, item) => {
+	acc.push({
+		[item.title]: item.descr,
 	});
-	
-	cardClone.querySelector('.product-card__price-val').textContent = `${String(card.price)[0]} ${String(card.price).split('').slice(1).join('')} ${card.currency}`;
-	
-	productCardWrapper.appendChild(cardClone);
-});
+
+	return acc;
+}, []);
+
+console.log(productProfile);
+// 1, 2, 3, 5 задания
+// Реализовать функцию, которая выводит (prompt) сообщение "Сколько карточек отобразить? От 1 до 5" и в зависимости от результата - будет выводить количество.
+// Должна быть защита от ввода других значений (проверка if). То-есть: у нас будет 2 функции, одна возвращает количество карточек, которое нужно ввести, другая - рендерить эти карточки (принимая массив аргументом)
+
+function getCardsNumber(userNumber) {
+	const cardsNumber = Number(userNumber);
+
+	if (!isFinite(cardsNumber) || cardsNumber <= 0) {
+		return 1;
+	}
+	return cardsNumber;
+}
+
+function renderCards(cardsArray, cardsNumber) {
+
+	const productCardsProfile = cardsArray.slice(0, cardsNumber);
+
+	productCardsProfile.forEach(card => {
+		const cardClone = productCardTemplate.content.cloneNode(true);
+
+		function getCardClone(element) {
+			return cardClone.querySelector(`.product-card__${element}`);
+		}
+
+		getCardClone('img').setAttribute('src', `images/${card.img}.png`);
+		getCardClone('img').setAttribute('alt', `${card.img}.png`);
+
+		getCardClone('title').textContent = card.title;
+		getCardClone('descr').textContent = card.descr;
+
+		card.structure.forEach(item => {
+			const li = document.createElement('li');
+			li.classList.add('product-card__structure-item');
+			li.textContent = item;
+			getCardClone('structure').appendChild(li);
+		});
+
+		getCardClone('price-val').textContent =
+			`${String(card.price)[0]} ${String(card.price).split('').slice(1).join('')} ${card.currency}`;
+
+		productCardWrapper.appendChild(cardClone);
+	});
+}
+
+
+const handleWindowLoad = () => {
+	const promtResult = prompt('Сколько карточек отобразить? От 1 до 5', 1);
+
+	const cardsNumber = getCardsNumber(promtResult);
+	renderCards(productCards, cardsNumber);
+}
+
+window.addEventListener('load', handleWindowLoad);
