7bc0b6a1559c69a01643761b1e5bab71bd49b2f8
Khao Kai <Ramza@gmail.com>
2026-01-29 17:25:07 +0300

    [12](chore): added html and css, redacting js file

--- js/Modal.js
+++ js/Modal.js
@@ -1,6 +1,31 @@
-
 export class Modal {
-	constructor(modalId) {
+	constructor(modalId, btnId) {
 		this.modal = document.getElementById(modalId);
+		this.overlay = document.getElementById('overlay');
+		this.#initOpen(btnId);
+	}
+
+	open() {
+		this.modal.classList.add('modal-showed');
+		this.overlay.classList.add('overlay-showed');
+	}
+
+	close() {
+		this.modal.classList.remove('modal-showed');
+		this.overlay.classList.remove('overlay-showed');
+	}
+
+	#initOpen(button) {
+		const btn = document.getElementById(button);
+		btn.addEventListener('click', () => {
+			this.open();
+		});
+	}
+
+	#initClose() {
+		const closeBtn = document.getElementById('modal-close-button');
+		closeBtn.addEventListener('click', () => {
+			this.close();
+		});
 	}
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+import { Modal } from "./Modal.js";
 
+const modalCloseButton = document.getElementById('modal-close-button');
+const modalRegistration = new Modal('registration-modal', 'registration-button');
+
+// const buttonAuth = document.getElementById('auth-button');
